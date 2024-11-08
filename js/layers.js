de53dfed5304709d4d87886939111a3cd0dd2a6f
16bitsCreator <preussenglorien@gmail.com>
2024-11-08 21:58:24 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -32,7 +32,7 @@ addLayer("m", {
 
     directMult() {
         let mult = new Decimal(1);
-        if (hasUpgrade("m", 21)) mult = mult.times(upgradeEffect("m", 21));
+        if (hasUpgrade("m", 22)) mult = mult.times(upgradeEffect("m", 22));
         return mult;
     },
 
@@ -43,7 +43,7 @@ addLayer("m", {
     upgrades: {
         11: {
             title: "Boost Production",
-            description: "Increase point generation based on Matter points.",
+            description: "Increases point generation based on Matter points.",
             cost: new Decimal(1),
             effect() {
                 return player[this.layer].points.add(1).pow(0.5);
@@ -52,21 +52,55 @@ addLayer("m", {
                 return "x" + format(this.effect()) + " to point generation"; 
             }
         },
-        21: {
-            title: "Boost Upgrade 13",
-            description: "Boosts the effect of Upgrade 13 based on reduced Matter points.",
-            cost: new Decimal(5),
+        12: {
+            title: "Matter Expansion",
+            description: "Boosts Matter point gain.",
+            cost: new Decimal(2),
             effect() {
-                return player[this.layer].points.add(1).pow(0.2);
+                let eff = new Decimal(2);
+                if (hasUpgrade("m", 22)) eff = eff.times(upgradeEffect("m", 22));
+                return eff;
             },
-            effectDisplay() {
-                return "x" + format(this.effect()) + " boost to Upgrade 13";
+            effectDisplay() { 
+                return "x" + format(this.effect()) + " to Matter point gain"; 
             }
         },
-        23: {
-            title: "Singularity",
-            description: "Allows to buy max Matter points",
-            cost: new Decimal(30),
+        13: {
+            title: "Accelerated Growth",
+            description: "Boosts point generation further.",
+            cost: new Decimal(5),
+            effect() {
+                let eff = new Decimal(3);
+                if (hasUpgrade("m", 21)) eff = eff.times(upgradeEffect("m", 21));
+                return eff;
+            },
+            effectDisplay() { 
+                return "x" + format(this.effect()) + " to point generation"; 
+            }
+        },
+        21: {
+            title: "Focused Matter",
+            description: "Boosts Upgrade 13 based on reduced Matter points.",
+            cost: new Decimal(10),
+            effect() {
+                let eff = player[this.layer].points.add(1).pow(0.5);  // Boost scales with inverse of Matter points
+                return eff;
+            },
+            effectDisplay() { 
+                return "x" + format(this.effect()) + " to Upgrade 13"; 
+            }
+        },
+        22: {
+            title: "Focused Points",
+            description: "Boosts Upgrade 12 based on reduced points.",
+            cost: new Decimal(10),
+            effect() {
+                let eff = player[this.layer].points.add(1).pow(0.25);  // Boost scales with inverse of Matter points
+                return eff;
+            },
+            effectDisplay() { 
+                return "x" + format(this.effect()) + " to Upgrade 13"; 
+            }
         },
     },
 });
